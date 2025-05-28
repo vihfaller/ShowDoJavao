c3b0eb70193ffd64dbd7c6730ac7ec4790eda3df
Vitória Faller Bier <vitoriafallerbier@gmail.com>
2025-05-28 14:30:12 -0300

    Update Jogo.java

--- Jogo/src/main/java/com/mycompany/jogo/Jogo.java
+++ Jogo/src/main/java/com/mycompany/jogo/Jogo.java
@@ -10,18 +10,5 @@ package com.mycompany.jogo;
  */
 public class Jogo {
     public static void main(String[] args) {
-
-        // Tela inicial
-        System.out.println("BEM-VINDO AO SHOW DO Javão!");
-        System.out.println("Responda com 'sim' para começar ou 'não' para sair.");
-        
-        String comecar = sc.nextLine();
-        
-        if (!comecar.equalsIgnoreCase("sim")) {
-            System.out.println("Então vamos começar!");
-        }else {
-            System.out.println("Tá bem, até a próxima!");
-            return;
-        }
 }
 }
